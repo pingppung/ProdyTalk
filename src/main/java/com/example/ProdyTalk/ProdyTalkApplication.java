0c3633cbaf3470e0df043de0c8f07ee8ab49e8cd
pingppung <57535999+pingppung@users.noreply.github.com>
2022-02-22 15:55:01 +0900

    Initial Commit
    react, spring boot 연동

---
+++ src/main/java/com/example/ProdyTalk/ProdyTalkApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProdyTalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdyTalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdyTalkApplication.class, args);
+	}
+
+}
