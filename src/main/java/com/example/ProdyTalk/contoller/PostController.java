cd0c08cc41a83f77748775f94d2af32fa40163de
pingppung <57535999+pingppung@users.noreply.github.com>
2022-03-03 03:09:55 +0900

    db 저장 후 페이지 이동
    회원가입 정보를 db에 저장 후 다시 메인 페이지로 이동

--- src/main/java/com/example/ProdyTalk/contoller/PostController.java
+++ src/main/java/com/example/ProdyTalk/contoller/PostController.java
@@ -3,10 +3,12 @@ package com.example.ProdyTalk.contoller;
 import com.example.ProdyTalk.mapper.UserMapper;
 import com.example.ProdyTalk.service.UserService;
 import com.example.ProdyTalk.vo.UserVO;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import lombok.RequiredArgsConstructor;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @CrossOrigin(origins="*",maxAge = 3600)
 @RestController
@@ -17,10 +19,16 @@ public class PostController {
     @Autowired
     UserMapper userMapper;
 
-    @PostMapping("/signup")
-    public String insertUser(UserVO user){
+    @PostMapping("/signup/result")
+    public void insertUser(UserVO user, HttpServletResponse response) throws IOException {
         userService.insertUser(user);
         System.out.println("유저 DB 저장 성공");
-        return "";
+        response.sendRedirect("http://localhost:3000/signup/result");
     }
+
+    @GetMapping("/signup/result")
+    public void success(){
+        System.out.println("회원가입 성공!!!");
+    }
+
 }
--- src/main/reactjs/src/App.js
+++ src/main/reactjs/src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 import Header from './components/Header'
 import Main from './pages/main/Main'
 import Signup from './pages/signup/Signup'
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Result from './pages/signup/Result'
+import { Route } from "react-router-dom";
 
 function App() {
+
   return (
     <>
            <Header />
            <Route exact path="/" component={Main} />
            <Route exact path="/signup" component={Signup} />
+           <Route exact path="/signup/result" component={Result} />
     </>
   );
 }
