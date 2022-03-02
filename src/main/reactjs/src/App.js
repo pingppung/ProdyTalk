76382dbf2282a7aee1a3cb7fdd34a95469116ac7
vladek99 <minhwa1224@naver.com>
2022-03-02 11:08:22 +0900

    회원가입 테스트

--- src/main/reactjs/src/App.js
+++ src/main/reactjs/src/App.js
@@ -1,39 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './components/Header'
+import Signup from './pages/signup/Signup'
 
 function App() {
-
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    fetch('/api/hello')
-      .then(response => response.text())
-      .then(message => {
-        setMessage(message);
-      });
-  },[])
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          {message}
-        </p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <Header />
+        <Signup />
+    </>
   );
 }
 
---
+++ src/main/reactjs/src/pages/signup/Signup.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Signup.css';
+
+function Signup() {
+    return (
+        <>
+            <form class="container" action="/user/create" method="post">
+                <div class="mb-3">
+                    <label class="form-label">아이디</label>
+                    <input type="text" class="form-control" name="user_id"></input>
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">비밀번호</label>
+                    <input type="text" class="form-control" name="user_pwd"></input>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+            </form>
+        </>
+    );
+}
+
+export default Signup;
