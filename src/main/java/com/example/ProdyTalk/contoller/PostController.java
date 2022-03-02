97d76fbe089e663c243126a40eff4bb2c411ca3a
Kyeoungeun Min <0406min@naver.com>
2022-03-02 13:43:03 +0900

    데베오류

---
+++ src/main/java/com/example/ProdyTalk/contoller/PostController.java
@@ -0,0 +1,21 @@
+package com.example.ProdyTalk.contoller;
+
+import com.example.ProdyTalk.mapper.UserMapper;
+import com.example.ProdyTalk.vo.UserVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins="*",maxAge = 3600)
+@RestController
+@RequestMapping("/")
+public class PostController {
+
+    @Autowired
+    UserMapper userMapper;
+
+    @PostMapping("/user/create")
+    void insertUser(UserVO user){
+        userMapper.insertUser(user);
+        System.out.println("유저 DB 저장 성공");
+    }
+}
---
+++ src/main/java/com/example/ProdyTalk/vo/UserVO.java
@@ -0,0 +1,13 @@
+package com.example.ProdyTalk.vo;
+
+import lombok.Data;
+
+@Data
+public class UserVO {
+
+    String user_name;
+    String user_id;
+    int user_pwd;
+    String user_email;
+    String user_phone;
+}
