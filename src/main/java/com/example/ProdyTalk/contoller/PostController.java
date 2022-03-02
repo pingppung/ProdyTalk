ccfa5288e063ed80577a49eec7e11e710ca7c49c
Kyeoungeun Min <0406min@naver.com>
2022-03-03 01:51:11 +0900

    디비 연결 성공!!

--- src/main/java/com/example/ProdyTalk/contoller/PostController.java
+++ src/main/java/com/example/ProdyTalk/contoller/PostController.java
@@ -1,21 +1,25 @@
 package com.example.ProdyTalk.contoller;
 
 import com.example.ProdyTalk.mapper.UserMapper;
+import com.example.ProdyTalk.service.UserService;
 import com.example.ProdyTalk.vo.UserVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import lombok.RequiredArgsConstructor;
+
 @CrossOrigin(origins="*",maxAge = 3600)
 @RestController
-@RequestMapping("/")
+@RequiredArgsConstructor
 public class PostController {
+    private final UserService userService;
 
     @Autowired
     UserMapper userMapper;
 
     @PostMapping("/signup")
     public String insertUser(UserVO user){
-        userMapper.insertUser(user);
+        userService.insertUser(user);
         System.out.println("유저 DB 저장 성공");
         return "";
     }
--- src/main/java/com/example/ProdyTalk/vo/UserVO.java
+++ src/main/java/com/example/ProdyTalk/vo/UserVO.java
@@ -7,7 +7,7 @@ public class UserVO {
 
     String user_name;
     String user_id;
-    int user_pwd;
+    String user_pwd;
     String user_email;
     String user_phone;
 }
