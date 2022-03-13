70f06d1d93b00f22498944febdd167ec5cf55504
vladek99 <minhwa1224@naver.com>
2022-03-13 21:10:28 +0900

    restart

--- src/main/java/com/example/ProdyTalk/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.example.ProdyTalk.service;
-
-import com.example.ProdyTalk.vo.UserVO;
-
-public interface UserService {
-    public void insertUser(UserVO user);
-    public UserVO findUser(UserVO user);
-}
