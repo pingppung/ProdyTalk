66a7ca7b8e173cf2d79d2478889055f2a1d3aeee
Kyeoungeun Min <0406min@naver.com>
2022-03-31 12:48:49 +0900

    방들어가기 버튼

--- src/main/reactjs/src/pages/MainPage.js
+++ src/main/reactjs/src/pages/MainPage.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import Room from '../components/Room'
+import Box from '@mui/material/Box';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
 import Header from '../components/HeaderComponent'
+import RoomCircle from '../components/room/RoomCircle.jsx'
+import RoomListComponent from '../components/RoomListComponent.jsx'
 function MainPage() {
     return (
         <>
             <Header />
-            <Room />
+            <br/><br/>
+            <Box sx={{ bgcolor: '#cfe8fc', height: '30vh' }}>
+                <div>
+                    <RoomListComponent />
+                </div>
+
+            </Box>
+
+
+
         </>
+
     );
 }
 
