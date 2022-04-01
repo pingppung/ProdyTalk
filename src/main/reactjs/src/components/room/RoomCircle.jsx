b175cff623c5c95a1c5957c58a657dbfe4695977
pingppung <57535999+pingppung@users.noreply.github.com>
2022-04-01 15:42:19 +0900

    방 입장시 url에 room_id 붙이기

--- src/main/reactjs/src/components/room/RoomCircle.jsx
+++ src/main/reactjs/src/components/room/RoomCircle.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 import { Link,withRouter } from "react-router-dom";
 
 function RoomCircle(props) {
-
+    console.log(props);
     return (
-    <Link to="/roomenter">
+    <Link to={`/roomenter/${props.data.room_id}`} >
         <label>
             <div id="circle">
-                <h3>{props.title}</h3>
+                <h3>{props.data.room_name}</h3>
             </div>
         </label>
     </Link>
