b37e2b6cd1c80b4cf85fe215b3529c9cf646b233
vladek99 <minhwa1224@naver.com>
2022-04-05 14:49:48 +0900

    server https 완료
    새로고침 안됨

---
+++ src/main/resources/static/precache-manifest.cc6a653620897fb62ac6c57436ac7aeb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d61625d1e68881d6c500e0b3b24b1083",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9e04f309c83b22d233b",
+    "url": "/static/css/2.ea593497.chunk.css"
+  },
+  {
+    "revision": "0f91589f6f2742ed92e9",
+    "url": "/static/css/main.e717fd9d.chunk.css"
+  },
+  {
+    "revision": "e9e04f309c83b22d233b",
+    "url": "/static/js/2.82f58bee.chunk.js"
+  },
+  {
+    "revision": "8daa3dd525309fad8d74",
+    "url": "/static/js/3.b2386893.chunk.js"
+  },
+  {
+    "revision": "0f91589f6f2742ed92e9",
+    "url": "/static/js/main.372a8914.chunk.js"
+  },
+  {
+    "revision": "276cd42cbece901cf973",
+    "url": "/static/js/runtime-main.01321137.js"
+  }
+]);
