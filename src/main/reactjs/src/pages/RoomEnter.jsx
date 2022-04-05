7d590195031c463b13a0b0bd0835efba35163d0b
Kyeoungeun Min <0406min@naver.com>
2022-04-05 16:58:27 +0900

    방 입장 -> 캘린더 조회, CalendarRoomVO 생성

--- src/main/reactjs/src/pages/RoomEnter.jsx
+++ src/main/reactjs/src/pages/RoomEnter.jsx
@@ -1,20 +1,33 @@
 import React, { useState, Component } from 'react';
 import {useLocation} from 'react-router-dom';
 import {useEffect} from 'react';
-import ChatPage from '../../pages/ChatPage';
+import ChatPage from './ChatPage';
+import Calendar from './Calendar.js';
 import {Button} from '@material-ui/core';
+import './css/RoomEnter.css';
 
 function RoomEnter() {
 
     const location=useLocation()
     const id=location.state
     const [chat,setChat]=useState(false)
+    const [calendar,setCalendar]=useState(false)
 
     const onChat = () => {
         if(chat===false){
             setChat(true)
+            setCalendar(false)
         }else {
             setChat(false);
+        }
+    }
+
+    const onCalendar = () => {
+        if(calendar==false){
+            setCalendar(true)
+            setChat(false)
+        }else{
+         setCalendar(false)
         }
     }
 
@@ -24,12 +37,15 @@ function RoomEnter() {
     return(
         <div>
             <Button>프로젝트 정보</Button>
-            <Button>팀별 캘린더</Button>
+            <Button variant="contained" color="primary" onClick={onCalendar}>팀별 캘린더</Button>
             <Button>파일공유</Button>
             <Button>화상채팅</Button>
             <Button variant="contained" color="primary" onClick={onChat}>그룹채팅</Button>
 
             {chat && <ChatPage id={id}/>}
+            <div className="calendar">
+                {calendar && <Calendar />}
+            </div>
         </div>
     )
 }
