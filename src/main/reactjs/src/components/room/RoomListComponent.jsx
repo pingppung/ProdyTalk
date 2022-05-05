a2f76aa73b8304bf6f1950f93f7587dcbd86dbb1
vladek99 <minhwa1224@naver.com>
2022-05-05 21:49:55 +0900

    code server test

--- src/main/reactjs/src/components/room/RoomListComponent.jsx
+++ src/main/reactjs/src/components/room/RoomListComponent.jsx
@@ -1,151 +1,55 @@
-import React,{useEffect} from 'react';
-import {useState} from 'react';
-import {Button} from '@material-ui/core';
-import SendIcon from '@mui/icons-material/Send';
-import SockJS from 'sockjs-client';
-import Stomp from 'stompjs';
-import Message from "./Message.js";
-import LeftMessage from "./LeftMessage.js";
-import Connected from "./Connected";
-import ChatService from "../../service/ChatService.js";
+import React,{ useState, useEffect } from 'react';
+import RoomService from '../../service/RoomService';
+import RoomCircle from './RoomCircle.jsx';
+import RoomCreate from './RoomCreate.jsx';
 import CircularProgress from '@mui/material/CircularProgress';
-import '../css/Chat.css';
-
-function PersonalChatComponent(props) {
-
-    const sock = new SockJS('http://localhost:8080/chat')
-    //const sock = new SockJS('https://pingppung.xyz:3000/chat')
-    const client=Stomp.over(sock);
-
-    const [list,setList]=useState(null)
-    const [id,setId]=useState(null);
-    const [userId,setUserId]=useState("");
-    const [messageId,setMessageId]=useState(0);
-    const [message,setMessage]=useState(null);
-    const [children, setChildren]=useState([]);
-    const [chatMessage,setChatMessage] = useState("");
-    const [changeNum,setChangeNum]=useState(1)
-
-    const changeMessage = (e) => {
-        setChatMessage(e.target.value);
-        }; //input 입력시 chatMessage state 변경
-
-    const onChangeNum = () => {
-        setChangeNum(changeNum+1)
-    }
-
-    const waitForConnection = (client, callback) => {
-        setTimeout(() => {
-            if(client.ws.readyState === 1) callback()
-            else waitForConnection(client,callback)
-        }, 1)
-    }
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import '../css/Room.css';
 
 
-    useEffect(()=>{
-        ChatService.getUserName().then(res => {
-            setId(res.data.id) //id에 token id 넣기'
-        })
+function RoomListComponent() {
 
-        ChatService.getPersonalChatList(props.id).then(res => {
-            setList(res.data)
-        })
-
-        client.connect({}, () =>{
-            client.subscribe('/queue/addChatToClient/personal/'+props.id, (messageVO) => { //
-                setUserId(JSON.parse(messageVO.body).user_id)
-                setMessage(JSON.parse(messageVO.body).content)
-                setMessageId(JSON.parse(messageVO.body).message_id)
-            }) //글 번호로 구독!
-        })
-
-        return () => {
-            client.disconnect(()=>{
-                client.unsubscribe('sub-0');
-            })
+    const [rooms,setRooms]=useState([])
+    const [loading,setLoading]=useState(false)
+    const [state,setState]=useState(false)
+    const settings = {
+          dots: true,
+          infinite: false,
+          speed: 500,
+          slidesToShow: 6,
+          slidesToScroll: 1,
+          centerPadding: '0px',  // 0px 하면 슬라이드 끝쪽 이미지가 안잘림
         }
-    }, [])
 
     useEffect(() => {
-        if(id!=null){
-            setChildren([
-                ...children,
-                <Connected id={id} />
-            ])
-        } else {
-            <CircularProgress />
-        }
-    },[id])
-
-    useEffect(() => {
-        if(list!=null){
-            const tempArray = []
-
-            list.forEach(listItem => {
-                if(listItem.user_id === id){
-                    tempArray.push([
-                        <Message id={listItem.user_id} content={listItem.content} />,
-                        <br /> ])
-                }else {
-                    tempArray.push([
-                        <LeftMessage userId={listItem.user_id} content={listItem.content} />,
-                        <br /> ])
-                }
-            })
-            setChildren([
-                ...children,
-                tempArray
-            ])
-
-
-        }
-    },[list])
-
-
-
-    const onChatMessage = () => {
-        waitForConnection(client,function() {
-            client.send(`/app/chat/personal/${props.id}`,{},JSON.stringify({'user_id':id, 'content':chatMessage}))
-            //conversationId로 메세지를 보내면서, user_id와 content, 방번호 포함
-            setChildren([
-                ...children,
-                <Message id={id} content={chatMessage} />, //보냄과 동시에 화면에 내가보낸 메시지 내용 오른쪽에 표시
-                <br />
-            ])
+        RoomService.getRooms().then((res) => {
+            setRooms(res.data);
+            setLoading(true)
         })
 
-        setChatMessage("")
-    }
-
-    useEffect(() => {
-        if(userId!=null&&message!=null){
-            if(userId!=id){
-                setChildren([
-                    ...children,
-                    <LeftMessage userId={userId} content={message} />, //메시지가 오면 왼쪽에 표시
-                    <br />
-                ])
-            }
-        }
-    },[messageId])
-
+    },[state]);
 
     return (
-        <div id="main-content" className="container">
-            <div className="row">
-                <div className="col-md-6">
-                    <div id="chatList">
-                        {children.map(child => child)}
-                    </div>
-                    <div id="inputMessage">
-                        <label>메시지 입력</label>
-                        <input className="obj" onChange={changeMessage} value={chatMessage} placeholder="message.." />
-                        <Button variant="contained" color="primary" onClick={onChatMessage}>보내기</Button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+    <div className="roomlist">
+        { loading === true
+         ? <div className="slider">
+            <Slider {...settings}>
+
+                {rooms.map(room =>
+                    <div><RoomCircle title={room.room_name} type={room.room_type} id={room.room_id}/></div>)
+                }
+
+            </Slider>
+          </div>
+         : <div className="circular"><CircularProgress /></div>
+
+         }
+
+    </div>
+    )
+
 }
 
-export default PersonalChatComponent;
+export default RoomListComponent;
