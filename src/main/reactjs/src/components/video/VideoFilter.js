bb8107c6cb19043e582bf1818273876a2becedc8
pingppung <57535999+pingppung@users.noreply.github.com>
2022-08-22 01:10:44 +0900

    얼굴필터 스티커 추가

--- src/main/reactjs/src/components/video/VideoFilter.js
+++ src/main/reactjs/src/components/video/VideoFilter.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
+import '../css/Filter.css';
 
 class VideoFilter extends Component {
+    constructor(props) {
+        super(props);
 
+   }
+    childFunction = (uri) => {
+       this.props.parentsFunc(uri);
+    }
     render() {
 
         const { open, header } = this.props;
@@ -13,12 +20,34 @@ class VideoFilter extends Component {
                       {header}
                     </header>
                     <main>
-                      카메라 변경
-                      얼굴 필터 설정
+                    <button onClick={() => this.childFunction()}>
+                        <img
+                              src="https://cdn.pixabay.com/photo/2013/07/13/09/51/unauthorised-156169_960_720.png"
+                              alt="new"
+                        />
+                      </button>
+                      <button onClick={() => this.childFunction('https://cdn.pixabay.com/photo/2017/09/30/09/29/cowboy-hat-2801582_960_720.png')}>
+                        <img
+                              src="https://cdn.pixabay.com/photo/2017/09/30/09/29/cowboy-hat-2801582_960_720.png"
+                              alt="new"
+                        />
+                      </button>
+                      <button onClick={() => this.childFunction('https://cdn.pixabay.com/photo/2015/12/11/06/37/santa-hat-1087709_960_720.png')}>
+                        <img
+                              src="https://cdn.pixabay.com/photo/2015/12/11/06/37/santa-hat-1087709_960_720.png"
+                              alt="new"
+                        />
+                      </button>
+                      <button onClick={() => this.childFunction('https://cdn.pixabay.com/photo/2014/12/21/23/52/crown-576226_960_720.png')}>
+                        <img
+                              src="https://cdn.pixabay.com/photo/2014/12/21/23/52/crown-576226_960_720.png"
+                              alt="new"
+                        />
+                      </button>
                     </main>
                     <footer>
-                      <button className="close">
-                        close
+                      <button className="apply">
+                        적용
                       </button>
                   </footer>
                   </section>
